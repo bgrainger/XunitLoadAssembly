254bfd9f098b83649033d85f3c1da533b986958d
Bradley Grainger <bgrainger@gmail.com>
2017-10-11 20:38:45 -0700

    Add failing example.

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
+using Xunit;
 
 namespace XunitLoadAssembly
 {
     public class Class1
     {
+        [Fact]
+        public void Test()
+        {
+            using (new MySqlConnection())
+            {
+            }
+        }
     }
 }
